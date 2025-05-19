86c36b7112dfc15b528493d9708cd0945dc97d81
Илья Сафронов <yaizkima@gmail.com>
2025-05-19 22:03:55 +0300

    bombardiro crocodilo

--- EmailNotificationService/src/main/java/com/icispp/emailnotificationservice/EmailNotificationServiceApplication.java
+++ EmailNotificationService/src/main/java/com/icispp/emailnotificationservice/EmailNotificationServiceApplication.java
@@ -2,8 +2,12 @@ package com.icispp.emailnotificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableKafka
 public class EmailNotificationServiceApplication {
 
     public static void main(String[] args) {
